bc5fac64e74f47555ef7fbc73f2d83b75d7f3610
leexizhe <leexizhe17@gmail.com>
2024-04-16 22:58:02 +0800

    coding

--- src/main/java/answers/arrays_hashing/TopKFrequentElements.java
+++ src/main/java/answers/arrays_hashing/TopKFrequentElements.java
@@ -5,27 +5,36 @@ import static common.PrintArray.printArray;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class TopKFrequentElements {
     private static int[] topKFrequent(int[] nums, int k) {
-        // time O(n*log(n)) where n=Length of nums
-        // space O(n)
-        // hashmap and priority queue
-
-        int[] ans = new int[k];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
-
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
-
-        for (Map.Entry<Integer, Integer> mapEntry : map.entrySet()) {
-            pq.add(mapEntry);
-            if (pq.size() > k) pq.poll();
+        /**
+         * Time Complexity: O(n * log k)
+         * Counting the frequency of each element in the array takes O(n) time, where n is the number of elements in the array.
+         * Constructing the Min Heap takes O(n * log k) time because we are adding each element to the heap and maintaining its size at most K.
+         * Building the result array takes O(k * log k) time because we extract each element from the heap (at most K times).
+         * Therefore, the overall time complexity is O(n + (n * log k) + (k * log k)), which simplifies to O(n * log k) since K is usually much smaller than n.
+         *
+         * Space Complexity: O(n + k)
+         * The space complexity of this solution is O(n) for storing the frequency of each element in the HashMap and O(k) for maintaining the Min Heap.
+         */
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for (int i = 0; i < k; i++) ans[i] = pq.poll().getKey();
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        for (int key : map.keySet()) {
+            heap.offer(key);
+        }
 
-        return ans;
+        int[] arr = new int[k];
+        for (int i = 0; i < k; i++) {
+            arr[i] = heap.poll();
+        }
+
+        return arr;
     }
 
     public static void main(String[] args) {
